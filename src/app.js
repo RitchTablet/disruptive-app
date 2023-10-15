f0d9a7038099324bc3997312375de572f5507e0a
ritchtablet <ritchtablet@gmail.com>
2023-10-14 20:01:21 -0600

    feat: more more more testing

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
+import express from "express";
 const app = express();
 
 import router from "./routes/routes.js";
-import cors from 'cors';
-import morgan from 'morgan';
-import logger from './config/logger.js';
-import { dataSource } from './database/typeorm.config.js';
+import cors from "cors";
+import morgan from "morgan";
+import logger from "./config/logger.js";
+import { dataSource } from "./database/typeorm.config.js";
 
 async function bootstrap() {
-  app.use(cors({ origin: '*' }));
-  app.use(morgan('combined'));
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+  app.use(cors({ origin: "*" }));
+  app.use(morgan("combined"));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-  app.use('/api', router);
+  app.use("/api", router);
 
   const port = process.env.PORT || 3001;
   await dataSource.initialize();
@@ -26,4 +26,4 @@ async function bootstrap() {
   });
 }
 
-bootstrap(); 
+bootstrap();
